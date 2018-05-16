ea99670d7b6473133bfa7e023f7c69a08bf8b52d
wanJunHao <1007300061@qq.com>
2018-05-16 18:18:29 +0800

    efwef

--- app_1/urls.py
+++ app_1/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path('index/', views.index),
+    path(r'index/', views.index),
+    re_path(r'^spend/(?P<method>\w+)/$', views.spend),
 ]
--- app_1/views.py
+++ app_1/views.py
@@ -1,10 +1,56 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from django.core.paginator import Paginator
 
-# Create your views here.
+import MySQLdb
 
 
+@api_view(["GET"])
 def index(request):
-	'''
-	'''
-	return HttpResponse("hello app1")
+    '''
+    '''
+    if request.method == 'GET':
+        return render(request, 'app_1/index.html')
+
+
+@api_view(["GET", "POST"])
+def spend(request, method):
+    '''
+    '''
+    conn = getConn()
+    c = conn.cursor(cursorclass=MySQLdb.cursors.DictCursor)
+    if request.method == 'GET':
+        if method == 'our':
+            sql = '''select * from spend'''
+
+    elif request.method == 'POST':
+        if method == 'our':
+            jsonData = request.data
+            sql = '''
+            select * from spend where DATE_FORMAT(in_date, '%Y-%m-%d') >= '{0}' and DATE_FORMAT(in_date, '%Y-%m-%d') <= '{1}'
+            '''.format(jsonData['start'], jsonData['end'])
+
+    print(sql)
+    c.execute(sql)
+    data = c.fetchall()
+
+    # 分页
+    paginator = Paginator(data, 14)
+    page = request.GET.get('page')
+    contacts = paginator.get_page(page)
+    count = 0
+    for i in data:
+        count += i['money']
+    context = {
+        "contacts": contacts,
+        "count": count
+    }
+    return render(request, 'app_1/spend.html', context)
+
+
+def getConn():
+    '''
+    '''
+    conn = MySQLdb.connect(host='localhost', port=3306, user='root', password='123.com', db='myspend', charset='utf8')
+    return conn
